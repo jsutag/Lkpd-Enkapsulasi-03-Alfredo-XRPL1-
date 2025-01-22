0ba5043d0f6205a3a285df50bb771841c8372158
jsutag <alfredoparluhutan09@gmail.com>
2025-01-22 11:18:26 +0700

    Update no6.php

--- no6.php
+++ no6.php
@@ -4,43 +4,36 @@ class ProdukElektronik {
     private $merk;
     private $garansi;
 
-    // Constructor untuk inisialisasi nilai awal
+
     public function __construct() {
         $this->merk = "";
-        $this->garansi = 0; // Garansi dalam bulan
+        $this->garansi = 0;
     }
 
-    // Setter untuk merk
     public function setMerk($merk) {
         $this->merk = $merk;
     }
 
-    // Getter untuk merk
     public function getMerk() {
         return $this->merk;
     }
 
-    // Setter untuk garansi
     public function setGaransi($garansi) {
         if ($garansi >= 0) {
             $this->garansi = $garansi;
         }
     }
 
-    // Getter untuk garansi
     public function getGaransi() {
         return $this->garansi;
     }
 }
 
-// Contoh penggunaan
 $produk = new ProdukElektronik();
 
-// Mengatur nilai menggunakan setter
 $produk->setMerk("Samsung");
 $produk->setGaransi(12);
 
-// Menampilkan nilai menggunakan getter
 echo "Merk Produk: " . $produk->getMerk() . "<br>";
 echo "Masa Garansi: " . $produk->getGaransi() . " bulan";
 
