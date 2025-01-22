a92c74a2512ce197d6b4c5e867d33b6b9c3f03e4
jsutag <alfredoparluhutan09@gmail.com>
2025-01-22 11:09:07 +0700

    Update no1.php

--- no1.php
+++ no1.php
@@ -4,35 +4,34 @@ class Produk {
     private $nama;
     private $harga;
 
-    // Setter untuk nama
+    
     public function setNama($nama) {
         $this->nama = $nama;
     }
 
-    // Getter untuk nama 
+     
     public function getNama() {
         return $this->nama;
     }
 
-    // Setter untuk harga
+    
     public function setHarga($harga) {
         $this->harga = $harga;
     }
 
-    // Getter untuk harga
+    
     public function getHarga() {
         return $this->harga;
     }
 }
 
-// Membuat objek dari kelas Produk
+
 $produk = new Produk();
 
-// Mengatur nilai menggunakan setter
+
 $produk->setNama("MSI Laptop");
 $produk->setHarga(10000000);
 
-// Menampilkan nilai menggunakan getter
 echo "Nama Produk: " . $produk->getNama() . "<br>";
 echo "Harga: Rp " . number_format($produk->getHarga(), 0, ',', '.');
 
