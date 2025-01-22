0e92dc050ae8b291b6fc74844eba9041d8a3d330
jsutag <alfredoparluhutan09@gmail.com>
2025-01-22 11:17:13 +0700

    Update no3.php

--- no3.php
+++ no3.php
@@ -3,12 +3,10 @@
 class BankAccount {
     private $saldo;
 
-    // Constructor untuk inisialisasi saldo awal
     public function __construct($saldoAwal = 0) {
         $this->saldo = $saldoAwal;
     }
 
-    // Metode untuk menyetor uang
     public function setor($jumlah) {
         if ($jumlah > 0) {
             $this->saldo += $jumlah;
@@ -18,7 +16,6 @@ class BankAccount {
         }
     }
 
-    // Metode untuk menarik uang
     public function tarik($jumlah) {
         if ($jumlah <= 0) {
             return "Jumlah penarikan harus lebih dari 0";
@@ -32,19 +29,18 @@ class BankAccount {
         return "Berhasil menarik Rp. " . number_format($jumlah) . ". Sisa saldo: Rp. " . number_format($this->saldo);
     }
 
-    // Getter untuk mendapatkan saldo
     public function getSaldo() {
         return $this->saldo;
     }
 }
 
-// Contoh penggunaan
-$akun = new BankAccount(1000000); // Membuat akun dengan saldo awal 1 juta
 
-// Melakukan beberapa transaksi
+$akun = new BankAccount(1000000); 
+
+
 echo $akun->setor(500000) . "<br>";
 echo $akun->tarik(300000) . "<br>";
-echo $akun->tarik(2000000) . "<br>"; // Mencoba menarik lebih dari saldo
+echo $akun->tarik(2000000) . "<br>"; 
 echo "Saldo akhir: Rp. " . number_format($akun->getSaldo());
 
 ?>
